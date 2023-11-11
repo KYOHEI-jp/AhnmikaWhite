8d2e62319c4bc1e2adcecb81e60123c1ae2c4578
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-11-11 22:27:11 +0900

    disable

--- app/src/main/java/com/example/anmikacolors/screen/ColorScreen.kt
+++ app/src/main/java/com/example/anmikacolors/screen/ColorScreen.kt
@@ -2,20 +2,17 @@ package com.example.anmikacolors.screen
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.example.anmikacolors.composable.WhiteBox
 import kotlinx.coroutines.delay
 
@@ -120,6 +117,11 @@ fun ColorScreen() {
                 )
             }
         }
+        // 広告を表示する関数
+        fun showAd() {
+            // ここに広告をロードし、表示するコードを記述します
+            // 例: AdMobのインタースティシャル広告を表示するコード
+        }
 
         Row(
             modifier = Modifier
@@ -131,10 +133,13 @@ fun ColorScreen() {
                 Button(onClick = {
                     showStartButton = false
                     resetQuiz()
+                    showAd() // 広告を表示する関数を呼び出す
                 }) {
                     Text("スタート")
                 }
             }
+
+
             Spacer(modifier = Modifier.width(8.dp))
             Button(onClick = { resetQuiz() }) {
                 Text("リセット")
