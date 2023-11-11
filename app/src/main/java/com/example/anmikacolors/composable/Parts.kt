36d254e1cd363d5414a30128dd9d6d24ef180fad
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-11-11 21:34:49 +0900

    Partsに切り出し

---
+++ app/src/main/java/com/example/anmikacolors/composable/Parts.kt
@@ -0,0 +1,31 @@
+package com.example.anmikacolors.composable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WhiteBox(color: Color, onClick: () -> Unit, showAnswer: Boolean) {
+    Box(
+        modifier = Modifier
+            .size(100.dp)
+            .padding(4.dp)
+            .border(1.dp, Color.Gray)
+            .background(color)
+            .clickable(onClick = onClick),
+        contentAlignment = Alignment.Center
+    ) {
+        if (showAnswer) {
+            Text("正解はこれでした。", color = Color.Black)
+        }
+    }
+}
