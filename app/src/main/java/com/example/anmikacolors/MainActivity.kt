76332e98547fc6ae30bf7b8fec9ae889fc224202
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-11-11 22:27:04 +0900

    不要なコード削除

--- app/src/main/java/com/example/anmikacolors/MainActivity.kt
+++ app/src/main/java/com/example/anmikacolors/MainActivity.kt
@@ -6,12 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.anmikacolors.screen.ColorScreen
 import com.example.anmikacolors.ui.theme.AnmikaColorsTheme
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,5 +29,30 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
+    private var mInterstitialAd: InterstitialAd? = null
+
+    private fun loadAd() {
+        val adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(
+            this,
+            "YOUR_ADMOB_AD_UNIT_ID",
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    mInterstitialAd = interstitialAd
+                }
+
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    mInterstitialAd = null
+                }
+            })
+    }
+
+    private fun showAd() {
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(this)
+        }
+    }
+}
