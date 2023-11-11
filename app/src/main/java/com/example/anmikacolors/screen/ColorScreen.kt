215a31f772ac5896201f2f7d26866c35ca59fb84
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-11-11 21:13:48 +0900

    スタートを押すとBoxとGridの白色が変わるようになった

--- app/src/main/java/com/example/anmikacolors/screen/ColorScreen.kt
+++ app/src/main/java/com/example/anmikacolors/screen/ColorScreen.kt
@@ -1,38 +1,38 @@
 package com.example.anmikacolors.screen
 
-import android.util.Log
-import android.widget.Button
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-
+import kotlin.random.Random
 
 @Composable
 fun ColorScreen() {
     // 30種類の異なる濃度の白色を生成
-    val shadesOfWhite = List(30) { i ->
+    val originalShadesOfWhite = List(30) { i ->
         val shade = 1f - (i / 29f * 0.3f)
         Color(shade, shade, shade)
     }
 
+    // 色のリストを状態として保持
+    var shadesOfWhite by remember { mutableStateOf(originalShadesOfWhite) }
+
     // 選ばれた色を状態として保持
-    var selectedColor by remember { mutableStateOf(shadesOfWhite[0]) }
+    var selectedColor by remember { mutableStateOf(Color.White) }
 
     // 色をシャッフルする関数
     fun shuffleColors() {
-        val shuffledColors = shadesOfWhite.shuffled()
-        selectedColor = shuffledColors[0] // シャッフル後の最初の色を選択
+        shadesOfWhite = originalShadesOfWhite.shuffled()
+        selectedColor = shadesOfWhite.random() // ランダムに選ばれた色を選択
     }
 
     Column(modifier = Modifier.fillMaxSize()) {
@@ -43,7 +43,7 @@ fun ColorScreen() {
                 .height(100.dp)
                 .background(selectedColor)
                 .padding(8.dp)
-                .border(1.dp, Color.Gray) // 罫線を追加
+                .border(1.dp, Color.Gray)
         )
 
         LazyVerticalGrid(
@@ -68,15 +68,13 @@ fun ColorScreen() {
     }
 }
 
-
-
 @Composable
 fun WhiteBox(color: Color) {
     Box(
         modifier = Modifier
             .size(100.dp)
             .padding(4.dp)
-            .border(1.dp, Color.Gray) // 罫線を追加
+            .border(1.dp, Color.Gray)
             .background(color)
     )
 }
@@ -86,15 +84,4 @@ fun StartButton(onClick: () -> Unit) {
     Button(onClick = onClick) {
         Text("スタート")
     }
-}
-
-@Composable
-fun ColorBox(color: Color) {
-    Box(
-        modifier = Modifier
-            .size(100.dp)
-            .padding(4.dp) // グリッドのセル間のスペースを調整
-            .border(1.dp, Color.Black) // 罫線を追加
-            .background(color)
-    )
 }
